a5216894bc27c1df27a76e93028b730dd462e811
Aayush-Everimit <aayush_2006@outlook.com>
2025-09-17 12:04:32 +0530

    Creating Suraksha SetuBackend

---
+++ surakshasetu-Backend/src/main/java/com/sih/surakshasetu/surakshasetu/SurakshasetuApplication.java
@@ -0,0 +1,13 @@
+package com.sih.surakshasetu.surakshasetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurakshasetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurakshasetuApplication.class, args);
+	}
+
+}
